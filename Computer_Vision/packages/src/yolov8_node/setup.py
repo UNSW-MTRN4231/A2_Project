615abed5264d8369f95d8794032dc68eb378fe03
UNSWAnton <z5362235@ad.unsw.edu.au>
2023-10-23 19:25:11 +1100

    Created Yolo Node

---
+++ Computer_Vision/packages/src/yolov8_node/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'yolov8_node'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'opencv-python',
+        'numpy',
+        'torch',
+    ],
+    zip_safe=True,
+    maintainer='mtrn',
+    maintainer_email='z5362235@ad.unsw.edu.au',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+             'yolo_node = yolov8_node.yolov8_object_detection:main'
+        ],
+    },
+)
