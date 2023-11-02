4a3e4382540aa85ce301b23d1e55afd6a7ba7032
Anton <antonle13342@gmail.com>
2023-11-02 18:36:49 +1100

    Radius + Centroid Topics Complete (Need Calibration)

--- Computer_Vision/packages/src/pizza_estimation/src/pizza_estimation.cpp
+++ Computer_Vision/packages/src/pizza_estimation/src/pizza_estimation.cpp
@@ -1,12 +1,13 @@
 #include "rclcpp/rclcpp.hpp"
 #include "yolov8_msgs/msg/detection_array.hpp"
 #include "sensor_msgs/msg/image.hpp"
-#include "geometry_msgs/msg/point.hpp"  // Include to publish centroid
-#include "std_msgs/msg/float64_multi_array.hpp"  // Homography matrix message type
+#include "geometry_msgs/msg/point.hpp"
+#include "std_msgs/msg/float64.hpp"  
 #include "cv_bridge/cv_bridge.h"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
+#include <vector>
+#include <numeric>
 
 class DetectionSubscriber : public rclcpp::Node
 {
@@ -17,29 +18,28 @@ public:
         subscription_ = this->create_subscription<yolov8_msgs::msg::DetectionArray>(
             "/yolo/old_detections", 10, std::bind(&DetectionSubscriber::callback, this, std::placeholders::_1));
         image_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "image_raw", 10, std::bind(&DetectionSubscriber::image_callback, this, std::placeholders::_1));
-        homography_subscription_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
-            "homography_matrix", 10, std::bind(&DetectionSubscriber::homography_callback, this, std::placeholders::_1));
-        
+            "image_warped", 10, std::bind(&DetectionSubscriber::image_callback, this, std::placeholders::_1));
+
         image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image_with_circles", 10);
-        centroid_publisher_ = this->create_publisher<geometry_msgs::msg::Point>("transformed_centroid", 10);
+        centroid_publisher_ = this->create_publisher<geometry_msgs::msg::Point>("pizza_centroid", 10);
+        radius_publisher_ = this->create_publisher<std_msgs::msg::Float64>("pizza_radius", 10);
+
+        robot_arm_position_ = cv::Point(-50, -250);
     }
 
 private:
     cv::Mat current_image_;
-    cv::Mat homography_matrix_;
+    cv::Point robot_arm_position_;
+    cv::Point centroid_point, radius_end;
+    geometry_msgs::msg::Point centroid_msg;
+    std_msgs::msg::Float64 radius_msg;
+    double avg_radius;
 
     void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
     {
         current_image_ = cv_bridge::toCvCopy(msg, "bgr8")->image;
     }
-
-    void homography_callback(const std_msgs::msg::Float64MultiArray::SharedPtr msg)
-    {
-        // Assume it's a 3x3 matrix for the homography
-        homography_matrix_ = cv::Mat(3, 3, CV_64F, msg->data.data());
-    }
-
+    
     void callback(const yolov8_msgs::msg::DetectionArray::SharedPtr msg)
     {
         if (current_image_.empty()) {
@@ -48,27 +48,22 @@ private:
         }
         
         cv::Mat image_with_circle = current_image_.clone();
-        
         for (const auto& detection : msg->detections) {
-            auto mask_centroid = computeCentroid(detection.mask);
-            cv::Point center(mask_centroid.first, mask_centroid.second);
-            cv::circle(image_with_circle, center, 5, cv::Scalar(255, 0, 0), -1);
-            
-            // Apply the homography matrix if available
-            if (!homography_matrix_.empty()) {
-                std::vector<cv::Point2f> original_points = {center};
-                std::vector<cv::Point2f> transformed_points;
-                cv::perspectiveTransform(original_points, transformed_points, homography_matrix_);
+            if (detection.class_id == 53) {
+                std::tie(avg_radius, centroid_point, radius_end) = computeEstimation(detection.mask);
 
-                geometry_msgs::msg::Point transformed_centroid_msg;
-                transformed_centroid_msg.x = transformed_points[0].x;
-                transformed_centroid_msg.y = transformed_points[0].y;
-                centroid_publisher_->publish(transformed_centroid_msg);
+                cv::circle(image_with_circle, centroid_point+robot_arm_position_, 5, cv::Scalar(0, 0, 255), -1); // centroid
+                cv::line(image_with_circle, centroid_point+robot_arm_position_, radius_end, cv::Scalar(255, 0, 0), 2); // radius line
+
+                centroid_msg.x = centroid_point.y;
+                centroid_msg.y = centroid_point.x;
+                centroid_msg.z = 0;
+                centroid_publisher_->publish(centroid_msg);
+
+                radius_msg.data = avg_radius;
+                radius_publisher_->publish(radius_msg);
             }
-
-            RCLCPP_INFO(this->get_logger(), "Mask Centroid: (%f, %f)", mask_centroid.first, mask_centroid.second);
         }
-        
         auto image_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", image_with_circle).toImageMsg();
         image_publisher_->publish(*image_msg);
     }
@@ -87,13 +82,26 @@ private:
         
         return {sum_x / count, sum_y / count};
     }
-
+    std::tuple<double, cv::Point, cv::Point> computeEstimation(const yolov8_msgs::msg::Mask& mask) const
+    {
+        std::vector<cv::Point> points;
+        for (const auto& point : mask.data) {
+            points.emplace_back(point.x, point.y);
+        }
+        cv::Rect bounding_rect = cv::boundingRect(points);
+        cv::Point center = (bounding_rect.tl() + bounding_rect.br()) / 2;
+        cv::Point top_center(bounding_rect.x + bounding_rect.width / 2, bounding_rect.y);
+        double avg_radius = cv::norm(top_center - center);
+        return std::make_tuple(avg_radius, center - robot_arm_position_, top_center);
+    }
     rclcpp::Subscription<yolov8_msgs::msg::DetectionArray>::SharedPtr subscription_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
-    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr homography_subscription_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
     rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr centroid_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr radius_publisher_;
 };
+
+
 
 int main(int argc, char * argv[])
 {
