a9afec3651ac97a2e96acad8e946ceb282f34c22
Jeet <JeetShah1513@protonmail.ch>
2023-11-03 01:04:08 +1100

    aruco poses found per tag

--- Computer_Vision/packages/src/image_warper/image_warper/aruco_detector.py
+++ Computer_Vision/packages/src/image_warper/image_warper/aruco_detector.py
@@ -4,7 +4,8 @@ from sensor_msgs.msg import Image
 import cv2
 import numpy as np
 from cv_bridge import CvBridge
-# from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose,PoseArray
+from std_msgs.msg import Float64MultiArray
 
 class ArucoDetector(Node):
 
@@ -12,8 +13,15 @@ class ArucoDetector(Node):
         super().__init__('aruco_detector')
         self.subscription = self.create_subscription(Image, 'image_raw', self.listener_callback, 10)
         self.publisher_ = self.create_publisher(Image, 'image_aruco_detected', 10) # TO CHANGE TO CORRECT TYPE
+        self.arucoMarkers_publisher = self.create_publisher(PoseArray,'markers_pose',10)
+
         self.startingAngles = -10
         self.angleChange = 0
+
+        self.tag0Pose = Pose()
+        self.tag1Pose = Pose()
+        self.tag2Pose = Pose()
+        self.tag3Pose = Pose()
         # self.pov_publisher_ = self.create_publisher(Float64MultiArray, 'warp_pov_tf', 10)
         # self.labelled_publisher_ = self.create_publisher(Image, 'image_labelled', 10)# New publisher
         #         image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image_with_circles", 10);
@@ -45,37 +53,60 @@ class ArucoDetector(Node):
                 # convert each of the (x, y)-coordinate pairs to integers
 
                 # note: these are relative to the original marker designs.
+                topRightF = (np.float64(topRight[0]), np.float64(topRight[1]))
+                # bottomRightF = (float(bottomRight[0]), float(bottomRight[1]))
+                # bottomLeftF = (float(bottomLeft[0]), float(bottomLeft[1]))
+                topLeftF = (np.float64(topLeft[0]), np.float64(topLeft[1]))
+
                 topRight = (int(topRight[0]), int(topRight[1]))
                 bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
                 bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
                 topLeft = (int(topLeft[0]), int(topLeft[1]))
-                
+
+                cX = np.float64((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = np.float64((topLeft[1] + bottomRight[1]) / 2.0)
+                angle = np.float64(np.arctan2((topRightF[1]-topLeftF[1]),(topRightF[0]-topLeftF[0])))
                 # ASSUMPTION: WE KNOW EXACTLY WHICH ARUCO MARKERS WE WORK WITH
                 if markerID == 0:
-                    self.angleChange = np.arctan2((topRight[1]-topLeft[1]),(topRight[0]-topLeft[0]))
-                    self.get_logger().info("Angle change recorded: " + str(self.angleChange))
+                    self.tag0Pose.position.x = cX
+                    self.tag0Pose.position.y = cY
+                    self.tag0Pose.orientation.w = angle
+                elif markerID == 1:
+                    self.tag1Pose.position.x = cX
+                    self.tag1Pose.position.y = cY
+                    self.tag1Pose.orientation.w = angle
+                elif markerID == 2:
+                    self.tag2Pose.position.x = cX
+                    self.tag2Pose.position.y = cY
+                    self.tag2Pose.orientation.w = angle
+                elif markerID == 3:
+                    self.tag3Pose.position.x = cX
+                    self.tag3Pose.position.y = cY
+                    self.tag3Pose.orientation.w = angle
+                else:
+                    self.get_logger().warn("No markers found")
                     #initialising first marker detection and its orientation
-                    if self.startingAngle == -10:
-                        self.startingAngle = self.angleChange
-                        self.angleChange = 0
-                        self.get_logger().info("Initialised starting angles: " + str(self.startingAngle))
+                    # if self.startingAngle == -10:
+                    #     self.startingAngle = self.angleChange
+                    #     self.angleChange = 0
+                    #     self.get_logger().info("Initialised starting angles: " + str(self.startingAngle))
                     
-                
-
                 cv2.line(labelled_image, topLeft, topRight, (0, 255, 0), 2)
                 cv2.line(labelled_image, topRight, bottomRight, (0, 255, 0), 2)
                 cv2.line(labelled_image, bottomRight, bottomLeft, (0, 255, 0), 2)
                 cv2.line(labelled_image, bottomLeft, topLeft, (0, 255, 0), 2)
 
-                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-                cv2.circle(labelled_image, (cX, cY), 4, (0, 0, 255), -1)
+                
+                cv2.circle(labelled_image, (int(cX), int(cY)), 4, (0, 0, 255), -1)
                 # draw the ArUco marker ID on the image
                 cv2.putText(labelled_image, str(markerID),(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         labelled_img_msg = self.bridge.cv2_to_imgmsg(labelled_image, 'bgr8')
         self.publisher_.publish(labelled_img_msg)
-        
+
+        markers_pose_msg = PoseArray()
+        markers_pose_msg.poses = [self.tag0Pose,self.tag1Pose,self.tag2Pose,self.tag3Pose]
+        self.arucoMarkers_publisher.publish(markers_pose_msg)
 
         # self.publisher_.publish(img_msg)
  
