6374730f1d2ddd6ff7372c19b1ef17b30f1eefda
anton <antonle13342@gmail.com>
2023-11-15 12:50:35 +1100

    build and run scripts done

--- runAll.sh
+++ runAll.sh
@@ -1,5 +1,3 @@
-colcon build
-source install/setup.bash
 cd launch/
 
 gnome-terminal -t "MoveitServer" -e 'ros2 launch ur_moveit_config ur_moveit.launch.py robot_ip:=192.168.0.100 ur_type:=ur5e launch_rviz:=true'
